ad6d0cd4284001e63c8e6c167fa3857e5d0caf88
Eduard Kudlanov <eduardokud@mail.ru>
2022-08-31 15:25:19 +0300

    last

---
+++ src/main/java/ru/netology/domen/PosterItem.java
@@ -0,0 +1,16 @@
+package ru.netology.domen;
+
+public class PosterItem {
+    private int id;
+    private String filmName;
+
+    public PosterItem(int id, String filmName) {
+        this.id = id;
+        this.filmName = filmName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+}
